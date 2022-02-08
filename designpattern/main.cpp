4b5c9c5cf8d571376c15d1c8980bdabef7163499
robin <lories1984@gmail.com>
2022-02-08 13:44:15 +0800

    责任链模式和策略模式

--- designpattern/main.cpp
+++ designpattern/main.cpp
@@ -7,6 +7,8 @@
 #include "graphicsimpl.h"
 #include "drawstuff.h"
 #include "dispatcher.h"
+#include "responsibilitychain.h"
+#include "strategy.h"
 
 using namespace std;
 using namespace DrawStuff;
@@ -57,7 +59,7 @@ void onUnKonwnMsgType(const MessagePtr& msg,
 
 int main()
 {
-    //test
+    //test swap
     {
         Graphics g1;
         Graphics g2;
@@ -72,18 +74,27 @@ int main()
         swap(test,test1);
     }
 
-    TestDispatch testDispatch;
-    MessageDispatcher dispatch(std::bind(&TestDispatch::onUnKonown, &testDispatch, std::placeholders::_1, std::placeholders::_2));
-    dispatch.registerMessageCallback<ChatMessage>(std::bind(&TestDispatch::onChat, &testDispatch, std::placeholders::_1, std::placeholders::_2));
-    dispatch.registerMessageCallback<LoginMessage>(std::bind(&TestDispatch::onLogin, &testDispatch, std::placeholders::_1, std::placeholders::_2));
-    dispatch.registerMessageCallback<ChatMessage>(onChatMsg);
-    dispatch.registerMessageCallback<LoginMessage>(onLoginMsg);
+    //消息分发
+    {
+        TestDispatch testDispatch;
+        MessageDispatcher dispatch(std::bind(&TestDispatch::onUnKonown, &testDispatch, std::placeholders::_1, std::placeholders::_2));
+        dispatch.registerMessageCallback<ChatMessage>(std::bind(&TestDispatch::onChat, &testDispatch, std::placeholders::_1, std::placeholders::_2));
+        dispatch.registerMessageCallback<LoginMessage>(std::bind(&TestDispatch::onLogin, &testDispatch, std::placeholders::_1, std::placeholders::_2));
+        dispatch.registerMessageCallback<ChatMessage>(onChatMsg);
+        dispatch.registerMessageCallback<LoginMessage>(onLoginMsg);
 
-    std::shared_ptr<ChatMessage> chat(new ChatMessage);
-    std::shared_ptr<LoginMessage> login(new LoginMessage);
-    dispatch.onProtobufMessage(chat, 11);
-    dispatch.onProtobufMessage(login, 22);
+        std::shared_ptr<ChatMessage> chat(new ChatMessage);
+        std::shared_ptr<LoginMessage> login(new LoginMessage);
+        dispatch.onProtobufMessage(chat, 11);
+        dispatch.onProtobufMessage(login, 22);
+    }
 
+    //设计模式
+    {
+        //行为模式
+        testStrategy();
+        testResponsibilityChain();
+    }
     system("pause");
 
     return 0;
---
+++ designpattern/responsibilitychain.h
@@ -0,0 +1,122 @@
+#pragma once
+#include <iostream>
+
+/**
+* 责任链模式是一种行为设计模式， 允许你将请求沿着处理者链进行发送。 收到请求后， 每个处理者均可对请求进行处理， 或将其传递给链上的下个处理者。
+* 值得注意的是， 请求可发送给链上的任意一个处理者， 而非必须是第一个处理者。
+* 还有一种稍微不同的方式 （也是更经典一种）， 那就是处理者接收到请求后自行决定是否能够对其进行处理。 如果自己能够处理， 处理者就不再继续传递请求。 
+* 因此在这种情况下， 每个请求要么最多有一个处理者对其进行处理， 要么没有任何处理者对其进行处理。 在处理图形用户界面元素栈中的事件时， 这种方式非常常见。
+**/
+
+/**
+ * The Handler interface declares a method for building the chain of handlers.
+ * It also declares a method for executing a request.
+ */
+class Handler {
+ public:
+  virtual Handler *SetNext(Handler *handler) = 0;
+  virtual std::string Handle(std::string request) = 0;
+};
+/**
+ * The default chaining behavior can be implemented inside a base handler class.
+ */
+class AbstractHandler : public Handler {
+  /**
+   * @var Handler
+   */
+ private:
+  Handler *next_handler_;
+
+ public:
+  AbstractHandler() : next_handler_(nullptr) {
+  }
+  Handler *SetNext(Handler *handler) override {
+    this->next_handler_ = handler;
+    // Returning a handler from here will let us link handlers in a convenient
+    // way like this:
+    // $monkey->setNext($squirrel)->setNext($dog);
+    return handler;
+  }
+  std::string Handle(std::string request) override {
+    if (this->next_handler_) {
+      return this->next_handler_->Handle(request);
+    }
+
+    return {};
+  }
+};
+/**
+ * All Concrete Handlers either handle a request or pass it to the next handler
+ * in the chain.
+ */
+class MonkeyHandler : public AbstractHandler {
+ public:
+  std::string Handle(std::string request) override {
+    if (request == "Banana") {
+      return "Monkey: I'll eat the " + request + ".\n";
+    } else {
+      return AbstractHandler::Handle(request);
+    }
+  }
+};
+class SquirrelHandler : public AbstractHandler {
+ public:
+  std::string Handle(std::string request) override {
+    if (request == "Nut") {
+      return "Squirrel: I'll eat the " + request + ".\n";
+    } else {
+      return AbstractHandler::Handle(request);
+    }
+  }
+};
+class DogHandler : public AbstractHandler {
+ public:
+  std::string Handle(std::string request) override {
+    if (request == "MeatBall") {
+      return "Dog: I'll eat the " + request + ".\n";
+    } else {
+      return AbstractHandler::Handle(request);
+    }
+  }
+};
+/**
+ * The client code is usually suited to work with a single handler. In most
+ * cases, it is not even aware that the handler is part of a chain.
+ */
+void ClientCode(Handler &handler) {
+  std::vector<std::string> food = {"Nut", "Banana", "Cup of coffee"};
+  for (const std::string &f : food) {
+    std::cout << "Client: Who wants a " << f << "?\n";
+    const std::string result = handler.Handle(f);
+    if (!result.empty()) {
+      std::cout << "  " << result;
+    } else {
+      std::cout << "  " << f << " was left untouched.\n";
+    }
+  }
+}
+/**
+ * The other part of the client code constructs the actual chain.
+ */
+int testResponsibilityChain() {
+  MonkeyHandler *monkey = new MonkeyHandler;
+  SquirrelHandler *squirrel = new SquirrelHandler;
+  DogHandler *dog = new DogHandler;
+  monkey->SetNext(squirrel)->SetNext(dog);
+
+  /**
+   * The client should be able to send a request to any handler, not just the
+   * first one in the chain.
+   */
+  std::cout << "Chain: Monkey > Squirrel > Dog\n\n";
+  ClientCode(*monkey);
+  std::cout << "\n";
+  std::cout << "Subchain: Squirrel > Dog\n\n";
+  ClientCode(*squirrel);
+
+  delete monkey;
+  delete squirrel;
+  delete dog;
+
+  return 0;
+}
